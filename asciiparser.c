d4d4dca89a496babc14c59ba3ddbb8e5f6d6b973
LeandroHuff <leandrohuff@programmer.net>
2022-03-18 13:33:08 -0300

    updates

---
+++ asciiparser.c
@@ -0,0 +1,13 @@
+#include "../Resources/defs.h"
+
+
+
+int asciiparser(char *str, char **sentences, char *div, int param)
+{
+  DEF_BUFFER(char, stmp, 128);
+
+
+
+
+
+}
---
+++ asciiparser.h
@@ -0,0 +1,12 @@
+
+
+#ifndef __ASCIIPARSER_H__
+#define __ASCIIPARSER_H__
+
+#include "../Resources/defs.h"
+
+
+
+
+
+#endif  // #ifndef __ASCIIPARSER_H__
