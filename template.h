6c6c3a2caf5a5123d54e51c46c06a1277a8b2d67
LeandroHuff <leandrohuff@programmer.net>
2022-03-21 22:51:47 -0300

    update

---
+++ template.h
@@ -0,0 +1,60 @@
+/**
+ * @brief       Short header description and its features.
+ * @file        filename.ext - Source code filename.
+ * @author      Author name - author e-mail
+ * @date        YYYY/MM/DD - Source code date.
+ * @version     VERSION.REVISION.BUILD - Source code version.
+ * @copyright   Disclaimer for copyrighted software.
+ */
+
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+
+/******************************************************************************
+    INCLUDES (LIBS/SYSTEM)
+******************************************************************************/
+
+
+/******************************************************************************
+    INCLUDES (USER)
+******************************************************************************/
+
+
+/******************************************************************************
+    DEFINES - FOR GLOBAL/SYSTEM USE
+******************************************************************************/
+
+
+/******************************************************************************
+    TYPEDEFS - FOR GLOBAL/SYSTEM USE
+******************************************************************************/
+
+
+/******************************************************************************
+    DATA - EXPORTED FOR GLOBAL/SYSTEM USE
+******************************************************************************/
+extern int data_exported;
+
+
+/******************************************************************************
+    FUNCTION PROTOTYPES - EXPORTED FOR GLOBAL/SYSTEM USE
+******************************************************************************/
+
+/**
+ * @brief           A short function description.
+ * @param Param1    First function parameter description.
+ * @param Param2    Second function parameter description.
+ * @param Param3    Third function parameter description.
+ * @return type     Code1 Return code 1 description.
+ *                  Code2 Return code 2 description.
+ *                  Code3 Return code 3 description.
+ */
+int function_ext( int Param1, int Param2, int Param3, ...);
+
+
+/******************************************************************************
+    END OF FILE - TEMPLATE.H
+******************************************************************************/
+
+#endif // TEMPLATE_H
